3088d88903b2fe7188c50905907d7ac82265cbe6
j4ndrw <murjan_andreialin@yahoo.com>
2023-07-09 22:49:44 +0300

    build: Added capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.qsmoke.j4ndrw',
+  appName: 'QSmoke',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
